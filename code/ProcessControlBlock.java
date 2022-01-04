37240fb9f14aa1a52601a49cff2087ba5fa76ee5
pantgram <pantgram@csd.auth.gr>
2022-01-04 21:47:17 +0200

    ProcessControlBlock is ready

--- code/ProcessControlBlock.java
+++ code/ProcessControlBlock.java
@@ -3,47 +3,52 @@ package code;
 import java.util.ArrayList;
 
 public class ProcessControlBlock {
-    
+
     private final int pid;
     private ProcessState state;
     // the following two ArrayLists should record when the process starts/stops
     // for statistical purposes
     private ArrayList<Integer> startTimes; // when the process starts running
     private ArrayList<Integer> stopTimes;  // when the process stops running
-    
-    private static int pidTotal= 0;
-    
+
+    private static int pidTotal = 0;
+
     public ProcessControlBlock() {
         this.state = ProcessState.NEW;
         this.startTimes = new ArrayList<Integer>();
         this.stopTimes = new ArrayList<Integer>();
-        /* TODO: you need to add some code here
-         * Hint: every process should get a unique PID */
-        this.pid = 0; // change this line
-        
+        //update the number of the total processes
+        pidTotal += 1;
+        this.pid = pidTotal;
+
     }
+
 
     public ProcessState getState() {
         return this.state;
     }
-    
+
     public void setState(ProcessState state, int currentClockTime) {
-        /* TODO: you need to add some code here
-         * Hint: update this.state, but also include currentClockTime
-         * in startTimes/stopTimes */
-        
+        //if a process starts
+        if (this.state == ProcessState.READY && state == ProcessState.RUNNING)
+            this.startTimes.add(currentClockTime);
+            //if a process stops
+        else if ((this.state == ProcessState.RUNNING && state == ProcessState.READY) || (this.state == ProcessState.RUNNING && state == ProcessState.TERMINATED))
+            this.stopTimes.add(currentClockTime);
+        //set state
+        this.state = state;
     }
-    
-    public int getPid() { 
+
+    public int getPid() {
         return this.pid;
     }
-    
+
     public ArrayList<Integer> getStartTimes() {
         return startTimes;
     }
-    
+
     public ArrayList<Integer> getStopTimes() {
         return stopTimes;
     }
-    
+
 }
