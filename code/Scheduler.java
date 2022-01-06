4b0197db67e9ef6ede3bcf58a5fd7ade98c3cd7b
Aristeidis Matskidis <arismats@csd.auth.gr>
2022-01-06 20:59:59 +0200

    Scheduler

--- code/Scheduler.java
+++ code/Scheduler.java
@@ -19,8 +19,7 @@ public abstract class Scheduler {
      * of processes that are candidates for execution. Common for all
      * schedulers. */
     public void removeProcess(Process p) {
-        /* TODO: you need to add some code here */
-        
+        processes.remove(p);
     }
     
     /* the getNextProcess() method should return the process that should
