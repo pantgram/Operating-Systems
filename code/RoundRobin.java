694c1289aad79b8337f30bf13242fb67405f246e
Aristea Lachana <alachana@csd.auth.gr>
2022-01-06 19:25:58 +0200

    class RoundRobin - might need changes

--- code/RoundRobin.java
+++ code/RoundRobin.java
@@ -3,10 +3,11 @@ package code;
 public class RoundRobin extends Scheduler {
 
     private int quantum;
+    private Process currentProcess;
     
     public RoundRobin() {
         this.quantum = 1; // default quantum
-        /* TODO: you _may_ need to add some code here */
+        currentProcess = null;
     }
     
     public RoundRobin(int quantum) {
@@ -14,13 +15,40 @@ public class RoundRobin extends Scheduler {
         this.quantum = quantum;
     }
 
+    public int getQuantum(){
+        return quantum;
+    }
+
     public void addProcess(Process p) {
-        /* TODO: you need to add some code here */
+        if(p.getPCB().getState() == ProcessState.READY || p.getPCB().getState() == ProcessState.RUNNING){
+            processes.add(p);
+        }
+        else{
+            System.out.println("The process isn't on \"READY\" or \"RUNNING\" state - can't be executed");
+        }
     }
     
     public Process getNextProcess() {
-        /* TODO: you need to add some code here
-         * and change the return value */
-        return null;
+        if(currentProcess == null){
+            currentProcess = processes.get(0);
+        }
+        else {
+            //index of the current process
+            int index = processes.indexOf(currentProcess);
+            Process previousProcess = currentProcess;
+            //TODO: previousProcess.reduceBurstTime - if needed
+
+            //new current process - the process to be returned
+            currentProcess = processes.get(index + 1);
+
+            //remove previous process from the start of line
+            processes.remove(previousProcess);
+
+            //if the -previous- process isn't TERMINATED, it gets at the end of the line
+            if(previousProcess.getPCB().getState() != ProcessState.TERMINATED) {
+                addProcess(previousProcess);//TODO: see what happens with quantum - clock?
+            }
+        }
+        return currentProcess;
     }
 }
