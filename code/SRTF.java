f18a657f4fb4b3f8f1814f700593defee0b046ac
Aristeidis Matskidis <arismats@csd.auth.gr>
2022-01-07 17:13:32 +0200

    SRTF

--- code/SRTF.java
+++ code/SRTF.java
@@ -15,7 +15,7 @@ public class SRTF extends Scheduler {
     }
     
     public Process getNextProcess() {
-        if(currentProcess == null || !processes.contains(currentProcess)){
+        if(currentProcess == null || !processes.contains(currentProcess)){ //check if currentProcess is null or if is not in arraylist processes
             currentProcess = processes.get(0);
         }
         else
@@ -23,7 +23,7 @@ public class SRTF extends Scheduler {
             int index = processes.indexOf(currentProcess);
             double min=99999;
             int new_index=0;
-            for(int i=index; i<=processes.size(); i++){
+            for(int i=index; i<=processes.size(); i++){ //next process in the arraylist is the one with the min response time in srtf
               if(processes.get(i).getResponseTime()<=min){
                   min = processes.get(i).getResponseTime();
                   new_index = i;
