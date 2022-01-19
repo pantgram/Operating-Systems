d8f9bbecd48c7781ae6f2aea66c2c2c5cfa33b40
Aristeidis Matskidis <arismats@csd.auth.gr>
2022-01-19 22:40:58 +0200

    SRTF

--- code/SRTF.java
+++ code/SRTF.java
@@ -3,13 +3,9 @@ package code;
 public class SRTF extends Scheduler {
 
     private Process currentProcess;
-    private Process previousProcess;
-    private int index;
 
     public SRTF() {
         this.currentProcess = null;
-        this.previousProcess = null;
-        this.index=0;
     }
 
     public void addProcess(Process p) {
@@ -23,57 +19,46 @@ public class SRTF extends Scheduler {
             currentProcess = processes.get(0);
             currentProcess.run();
             currentProcess.setBurstTime(currentProcess.getBurstTime()-1);
-            index++;
-        }
-        else if(previousProcess == null){  //second time running
-            previousProcess=currentProcess;
-            currentProcess = processes.get(index);
-            index++;
-            if(currentProcess.getBurstTime()<=previousProcess.getBurstTime()) {
-                currentProcess.run();
-                currentProcess.setBurstTime(currentProcess.getBurstTime() - 1);
-            }
-        }
-        else if(currentProcess.getBurstTime()==0){// if the process ended
-            previousProcess = currentProcess;
-            removeProcess(currentProcess);
-            currentProcess = nextProcessExists();
-        }
-        else if(currentProcess.getBurstTime()<=previousProcess.getBurstTime()){// checking if remaining time of curr process is less than prev
-            currentProcess.run();
-            currentProcess.setBurstTime(currentProcess.getBurstTime()-1);
-            previousProcess=currentProcess;
-            currentProcess = nextProcessExists();
-            index++;
-        }
-        else{
-            currentProcess.run();
-            currentProcess.setBurstTime(currentProcess.getBurstTime()-1);
-        }
-        if (currentProcess != null) {
-            currentProcess.setBurstTime(currentProcess.getBurstTime() - 1);
             System.out.println("RUNNING " + currentProcess.getPCB().getPid() + " IN " + CPU.clock);
         }
-        else{System.out.println("No process to be executed");}
+        else if(currentProcess.getBurstTime() !=0) {
+            Process minProcess = minBurstTime();
+            if(currentProcess == minProcess){
+                currentProcess.setBurstTime(currentProcess.getBurstTime()-1);
+                System.out.println("RUNNING " + currentProcess.getPCB().getPid() + " IN " + CPU.clock);
+            }
+            else{
+                currentProcess = minProcess;
+                currentProcess.run();
+                currentProcess.setBurstTime(currentProcess.getBurstTime()-1);
+                System.out.println("RUNNING " + currentProcess.getPCB().getPid() + " IN " + CPU.clock);
+            }
+        }
+        else{
+          removeProcess(currentProcess);
+          if(processes.size()>0) {
+              currentProcess = minBurstTime();
+              currentProcess.run();
+              currentProcess.setBurstTime(currentProcess.getBurstTime() - 1);
+              System.out.println("RUNNING " + currentProcess.getPCB().getPid() + " IN " + CPU.clock);
+          }else{System.out.println("No processes to be executed");}
+        }
         return currentProcess;
     }
 
-    private Process nextProcessExists() {
-        if (index <= processes.size() - 1) { //if next element of arraylist exists
-            currentProcess = processes.get(index);
-            if(previousProcess != currentProcess && previousProcess!=null)
-                currentProcess.run();
-            return processes.get(index);
-        } else if (processes.size() > 0) { //get it from the start
-            index = 0;
-            currentProcess = processes.get(0);
-            if(previousProcess != currentProcess && previousProcess!=null)
-                currentProcess.run();
-            return processes.get(0);
-        }
-        return null; //no more processes to be executed
-    }
 
+    private Process minBurstTime(){ //Process with min burst time
+       int min = Integer.MAX_VALUE;
+       Process minProcess = processes.get(0);
+       for(Process process : processes){
+           if(process.getBurstTime()<=min)
+           {
+               min=process.getBurstTime();
+               minProcess = process;
+           }
+       }
+       return minProcess;
+    }
 
     public static void main(String[] args) {
         final Process[] processes = {
@@ -81,17 +66,18 @@ public class SRTF extends Scheduler {
                 new Process(0, 7, 10),
                 new Process(1, 3, 40),
                 new Process(3, 4, 25),
-                new Process(6, 2, 30)
         };
         Scheduler SRTFTEST = new SRTF();
         processes[0].getPCB().setState(ProcessState.READY, 0);
         processes[1].getPCB().setState(ProcessState.READY, 0);
         processes[2].getPCB().setState(ProcessState.READY, 0);
-        processes[3].getPCB().setState(ProcessState.READY, 0);
         SRTFTEST.addProcess(processes[0]);
         SRTFTEST.addProcess(processes[1]);
         SRTFTEST.addProcess(processes[2]);
-        SRTFTEST.addProcess(processes[3]);
+        SRTFTEST.getNextProcess();
+        SRTFTEST.getNextProcess();
+        SRTFTEST.getNextProcess();
+        SRTFTEST.getNextProcess();
         SRTFTEST.getNextProcess();
         SRTFTEST.getNextProcess();
         SRTFTEST.getNextProcess();
