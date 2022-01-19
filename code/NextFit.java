93454f057a6ad65ba618b3fe6901f38eb0b3cbdb
Aristeidis Matskidis <arismats@csd.auth.gr>
2022-01-19 21:07:50 +0200

    NextFit

--- code/NextFit.java
+++ code/NextFit.java
@@ -11,10 +11,6 @@ public class NextFit extends MemoryAllocationAlgorithm {
     public int fitProcess(Process p, ArrayList<MemorySlot> currentlyUsedMemorySlots) {
         boolean fit = false;
         int address = -1;
-        /* TODO: you need to add some code here
-         * Hint: this should return the memory address where the process was
-         * loaded into if the process fits. In case the process doesn't fit, it
-         * should return -1. */
 
         int blockLength = this.availableBlockSizes.length;// length of array
 
