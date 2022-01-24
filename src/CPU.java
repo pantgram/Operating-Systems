64b4c0049e076290dec03fb5747b2c83199a2f1b
Aristea Lachana <alachana@csd.auth.gr>
2022-01-24 12:52:29 +0200

    comments - RR and CPU

--- src/CPU.java
+++ src/CPU.java
@@ -8,7 +8,6 @@ public class CPU {
     private Scheduler scheduler;
     private MMU mmu;
     private Process[] processes;
-    private int currentProcess;
 
     public CPU(Scheduler scheduler, MMU mmu, Process[] processes) {
         this.scheduler = scheduler;
@@ -16,6 +15,9 @@ public class CPU {
         this.processes = processes;
     }
 
+    /**
+     * Call tick() method for each clock cycle - call checkToStop() to terminate.
+     */
     public void run() {
         while (checkToStop()) {
             tick();
@@ -23,17 +25,27 @@ public class CPU {
         }
     }
 
+    /**
+     * Check if there are processes to be removed. Then, decide if the current process continues to execute,
+     * or get next process for execution - it depends on the scheduler.
+     * Also, check if there are processes to load.
+     */
     public void tick() {
         /* Checking if a Terminated Process needs to be removed from RAM */
         removeTerminatedProcesses();
 
-        // If the SRTF algorithm is being used or no Processes have been added to the processes ArrayList
-        // processToLoad is called. If a process is stored in RAM the current CPU cycle is stopped
+        /* If the SRTF algorithm is being used or no Processes have been added to the processes ArrayList
+           processToLoad is called. If a process is stored in RAM the current CPU cycle is stopped.
+         */
         if (isSRTF() || scheduler.processes.size() == 0) {
             if (processToLoad())
                 return;
         }
 
+        /*
+        When current process p gets null value, it is time to check if there is
+        another process to load.
+         */
         if (scheduler.processes.size() > 0) {
             Process p = scheduler.getNextProcess();
             if(p == null && isRR())
@@ -62,6 +74,9 @@ public class CPU {
         return this.scheduler.getClass().getName().equals("RoundRobin");
     }
 
+    /**
+     * delete processes that are terminated and exist in RAM
+     */
     private void removeTerminatedProcesses() {
         for (Process process : this.processes) {
             if (process.getPCB().getState().equals(ProcessState.TERMINATED) && processIsInRAM(process)) {
@@ -78,6 +93,10 @@ public class CPU {
         return !this.terminatedProcesses.contains(process);
     }
 
+    /**
+     * delete given process that is terminated and exists in RAM
+     * @param terminatedProcess given process
+     */
     private void removeTerminatedProcess(Process terminatedProcess) {
         for (MemorySlot slot : this.mmu.getCurrentlyUsedMemorySlots()) {
             if (slot.getProcess().equals(terminatedProcess)) {
@@ -88,6 +107,10 @@ public class CPU {
         }
     }
 
+    /**
+     * @return true if there are no terminated processes, false if there is at
+     * least one terminated process
+     */
     private boolean checkToStop() {
         for (Process process : this.processes) {
             if (!process.getPCB().getState().equals(ProcessState.TERMINATED))
@@ -96,6 +119,10 @@ public class CPU {
         return false;
     }
 
+    /**
+     * Add one process, which state is NEW, to arraylist processes when it is necessary.
+     * @return true when at least one process is added in arraylist processes
+     */
     private boolean processToLoad() {
         for (Process process : this.processes) {
             if (process.getArrivalTime() <= CPU.clock && process.getPCB().getState().equals(ProcessState.NEW)) {
--- src/RoundRobin.java
+++ src/RoundRobin.java
@@ -3,7 +3,7 @@ public class RoundRobin extends Scheduler {
     private int quantum;
     private Process currentProcess;
     private Process previousProcess;
-    private int pointer;
+    private int pointer; //it shows the index of process element - current process
     private int reachQuantum; //pointer to check if we reached the quantum limit
     private boolean f; //it gets true if the process reaches quantum or gets TERMINATED
 
@@ -29,6 +29,7 @@ public class RoundRobin extends Scheduler {
 
 
     public Process getNextProcess() {
+        //if currentProcess reached quantum or got TERMINATED, it's time to get the next process
         if (f) {
             currentProcess = nextProcessExists();
             f = false;
@@ -43,9 +44,9 @@ public class RoundRobin extends Scheduler {
             pointer = 0;
         }
 
-
         reachQuantum++;
 
+        //reduce burstTime and print
         if (currentProcess != null) {
             currentProcess.setBurstTime(currentProcess.getBurstTime() - 1);
             System.out.println("Process " + currentProcess.getPCB().getPid() + " - running in clock cycle: " + CPU.clock);
@@ -55,10 +56,8 @@ public class RoundRobin extends Scheduler {
         if (currentProcess.getBurstTime() == 0) {
             previousProcess = currentProcess;
             removeProcess(currentProcess);
-            //get next process - if it exists
-            //currentProcess = nextProcessExists();
             f = true;
-            return null;
+            return null; //CPU will check for new processes to load
         }
 
         //if it reaches the quantum limit, get the next process - if it exists
@@ -68,16 +67,23 @@ public class RoundRobin extends Scheduler {
             previousProcess = currentProcess;
             if (currentProcess != null)
                 currentProcess.waitInBackground();
-            //currentProcess = nextProcessExists();
             f = true;
-            return null;
+            return null; //CPU will check for new processes to load
         }
 
-        //we return null if f=true, because in CPU, when this method returns null,
-        //it is time to check if there is any process to load in memory
+        /* return null, because in CPU, when this method returns null,
+           it is time to check if there is any process to load in memory,
+           else return the current Process
+         */
         return currentProcess;
     }
 
+    /**
+     * Check if there is an element after the currentProcess element - in processes arraylist.
+     * If there is that element, that's the next process to execute. If not, start again
+     * the execution from the first element of processes. Those happen when processes.size()>0.
+     * @return next process to be executed or null (no more processes)
+     */
     private Process nextProcessExists() {
         if (pointer <= processes.size() - 1) { //if next element of arraylist exists
             reachQuantum = 0;
